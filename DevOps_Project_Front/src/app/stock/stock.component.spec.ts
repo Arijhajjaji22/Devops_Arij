5b6fc70913fa592c4dd1697aa144b5a174ed616d
Arijhajjaji22 <arij.hajjaji@esprit.tn>
2023-11-03 20:48:25 +0100

    mise a jour

--- DevOps_Project_Front/src/app/stock/stock.component.spec.ts
+++ DevOps_Project_Front/src/app/stock/stock.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { StockComponent } from './stock.component';
 
@@ -8,7 +9,9 @@ describe('StockComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [StockComponent]
+	 declarations: [StockComponent],
+	imports: [HttpClientTestingModule],
+     
     });
     fixture = TestBed.createComponent(StockComponent);
     component = fixture.componentInstance;
