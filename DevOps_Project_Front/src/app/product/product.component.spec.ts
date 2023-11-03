ab41c6ffedb28c0d9000426257d625cc3e5704c2
Arijhajjaji22 <arij.hajjaji@esprit.tn>
2023-11-03 20:52:16 +0100

    mise a jour

--- DevOps_Project_Front/src/app/product/product.component.spec.ts
+++ DevOps_Project_Front/src/app/product/product.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProductComponent } from './product.component';
 
@@ -8,7 +9,8 @@ describe('ProductComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductComponent]
+      declarations: [ProductComponent],
+	  imports: [HttpClientTestingModule], 
     });
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
